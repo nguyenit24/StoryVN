51efabd25dfe40f933754a4589bae0c94f20d445
HDNIT3 <nguyenhuynh.663459@gmail.com>
2026-09-07 23:41:29 +0700

    fix: set apiUrl to /api

--- frontend/src/config/env.config.ts
+++ frontend/src/config/env.config.ts
@@ -1,4 +1,7 @@
 export const envConfig = {
-    apiUrl: process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
-    port: process.env.PORT || 5000,
+  apiUrl:
+    typeof window !== "undefined"
+      ? "/api"
+      : process.env.NEXT_PUBLIC_API_URL || "http://localhost:3000/api",
+  port: process.env.PORT || 5000,
 };
